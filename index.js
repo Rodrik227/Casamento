7c8ba613eb36c35be1250199933f023224a7456f
Rodrik227 <gamer11dini@gmail.com>
2023-04-10 20:56:06 -0300

    Site completo de casamento

---
+++ index.js
@@ -0,0 +1,11 @@
+function copiarTexto(){
+    const content = document.getElementById('cpf').innerHTML;
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+}
